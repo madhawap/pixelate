73153f9b1a497fbb06880df11a85d43566275aff
Madhawa Perera <madhawaperera@mrbeanz-mac.local>
2023-05-10 14:11:32 +1000

    Add functionality to reverse the pixelation

--- pixelmagic.py
+++ pixelmagic.py
@@ -20,7 +20,8 @@ except ImportError:
     from PIL import Image
 
 
-def create_pixelated_gif(image_path: str, duration: float, loops: int, start_pixel_size: int = 20, output_path: str = '') -> str:
+def create_pixelated_gif(image_path: str, duration: float, loops: int, start_pixel_size: int = 20,
+                         output_path: str = '', reverse: bool = False) -> str:
     """Create a pixelated GIF from an image file and save it to disk.
 
     Args:
@@ -29,6 +30,8 @@ def create_pixelated_gif(image_path: str, duration: float, loops: int, start_pix
         loops (int): The number of times the GIF should loop. Use 0 for infinite loops.
         start_pixel_size (int): The starting pixel size of the pixelation effect. Default is 20.
         output_path (str): The path to the output file. If not specified, the GIF will be saved to the root directory.
+        reverse (bool): If True, the function will create a GIF that transitions from the pixelated image to the
+        original image.
 
     Returns:
         str: The path to the output file. The pixelated GIF is saved to disk.
@@ -53,22 +56,38 @@ def create_pixelated_gif(image_path: str, duration: float, loops: int, start_pix
     # Create a list to hold the frames of the GIF
     frames = []
 
-    # Apply the pixelation effect to the image and add it to the frames list
-    for pixel_size in range(start_pixel_size, end_pixel_size - 1, -1):
-        if pixel_size == 0:
-            continue
+    if not reverse:
+        # Add the non-pixelated image to the frames list
+        frames.append(image)
 
-        # Resize the image to the pixelated size
-        small_image = image.resize((image.width // pixel_size, image.height // pixel_size), resample=Image.BOX)
+        # Apply the pixelation effect to the image and add it to the frames list
+        for pixel_size in range(end_pixel_size + 1, start_pixel_size):
+            # Resize the image to the pixelated size
+            small_image = image.resize((image.width // pixel_size, image.height // pixel_size), resample=Image.BOX)
 
-        # Scale the pixelated image back up to its original size
-        pixelated_image = small_image.resize(image.size, resample=Image.NEAREST)
+            # Scale the pixelated image back up to its original size
+            pixelated_image = small_image.resize(image.size, resample=Image.NEAREST)
 
-        # Add the pixelated image to the frames list
-        frames.append(pixelated_image)
+            # Add the pixelated image to the frames list
+            frames.append(pixelated_image)
 
-    # Add the non-pixelated image to the frames list
-    frames.append(image)
+    else:
+        # Apply the pixelation effect to the image and add it to the frames list
+        for pixel_size in range(start_pixel_size, end_pixel_size - 1, -1):
+            if pixel_size == 0:
+                continue
+
+            # Resize the image to the pixelated size
+            small_image = image.resize((image.width // pixel_size, image.height // pixel_size), resample=Image.BOX)
+
+            # Scale the pixelated image back up to its original size
+            pixelated_image = small_image.resize(image.size, resample=Image.NEAREST)
+
+            # Add the pixelated image to the frames list
+            frames.append(pixelated_image)
+
+        # Add the non-pixelated image to the frames list
+        frames.append(image)
 
     # Set the output file path
     if not output_path:
