7355d4d089a8e5d8bd6f8c17bdc6cc9851192dd5
Madhawa Perera <madhawaperera@mrbeanz-mac.local>
2023-05-10 14:12:17 +1000

    Increase version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pixelmagic',
-    version='0.1.0',
+    version='1.0.0',
     author='madhawap',
     author_email='madhawa.perera@anu.edu.au',
     description='A package for creating pixelated GIFs from images',
